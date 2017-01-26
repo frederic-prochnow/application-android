e74cfa0571b4bb755a88a670169bdecda20775b7
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2017-01-26 15:54:09 +0100

    2601171553 primes2

---
+++ Prems2/app/src/main/java/fr/univ_lille1/iut_info/prochnof/prems2/MainActivity.java
@@ -0,0 +1,43 @@
+package fr.univ_lille1.iut_info.prochnof.prems2;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void doOk(View view) {
+        TextView tvNom;
+        TextView tvMessage;
+        String msg;
+        tvNom = (TextView) findViewById(R.id.nom);
+        tvMessage = (TextView) findViewById(R.id.message);
+        msg ="Bienvenue " + tvNom.getText() + "!";
+        tvMessage.setText(msg);
+    }
+}
